1eca20d75def11959f6b9693624f58565365cfc9
Krevo <francois@crevola.org>
2015-09-23 13:40:17 +0200

    Add tool for decrypting Urwhigo strings

---
+++ readustring.php
@@ -0,0 +1,99 @@
+<?php 
+
+  function extractStringFromLine($str)  {
+    $startIndex = strpos($str,'"');
+    $endIndex = strrpos($str,'"');
+    return substr($str,$startIndex+1,$endIndex-$startIndex-1);
+  }
+
+  function luaStringToPhpString($luaStr) {
+    $match = array();
+    $res = preg_match_all("/\\\\[0-9]{3}/",$luaStr,$match);
+    $search[] = "\\b"; $repl[] = "\x8"; // backslash
+    $search[] = "\\v"; $repl[] = "\xB"; // vertical tab
+    $search[] = "\\a"; $repl[] = "\xA"; // bell
+    $search[] = "\\t"; $repl[] = "\t"; // tab
+    $search[] = "\\r"; $repl[] = "\r"; 
+    $search[] = "\\n"; $repl[] = "\n";
+    $search[] = "\\f"; $repl[] = "\f";
+    $search[] = "\\\\"; $repl[] = "\\";  
+    $search[] = "\\\""; $repl[] = "\"";
+    foreach($match[0] as $matched) {
+    	$code = substr($matched,1,3);
+    	$search[] = $matched;
+        $repl[] = chr(intval($code));
+    }
+    return str_replace($search,$repl,$luaStr);
+  }
+
+/*
+function _98v(str)
+  local res = ""
+  local dtable = "\024D}\005\tC)G\016;.\f&wL<i\021\027ht,7d_ Bb]Y\030Px~\nJm\004f5\002\017%V!j#0{9\\(qr@4vg\020uS\028e>XQ\019\025ZU\018HM\v\"Oy\014'n3/[+pz\022*:2I\b\rNF\006\026`kl1=s?\000W\aAR\0316oc\001\003\023\029^a-E8\015T$|K"
+  for i = 1, #str do
+    local b = str:byte(i)
+    if b > 0 and b <= 127 then
+      res = res .. string.char(dtable:byte(b))
+    else
+      res = res .. string.char(b)
+    end
+  end
+  return res
+end
+*/
+
+  function udecrypt($str,$dtable) {
+    $res = "";
+    for ($i=0; $i<strlen($str); $i++) {
+      $b = ord($str[$i]);
+      if ($b > 0 && $b <= 127) {
+        $res .= $dtable[$b-1];
+      } else {
+        $res .= $str[$i];
+      }
+    }
+    return $res;
+  }
+
+  // Check number of arguments
+  if (count($argv)<2) {
+    echo "Usage: php $argv[0] lua_source_file > lua_source_file_decrypted".PHP_EOL;
+    die;
+  }
+
+  // Open file
+  $filename = $argv[1];
+  $fh = @fopen($filename,"rb");
+  if ($fh === FALSE) {
+    echo "Unable to open $filename !".PHP_EOL;
+    die;
+  }
+  
+  $functionStringCryptPattern = "/function (_[a-zA-Z0-9]{3})\\(/";
+  $match = array();
+  $dtable = "";
+  $LOOK_FOR_DTABLE = false;
+  $LOOK_FOR_FNAME = true;
+  $uncryptStringFunctionName = "____";
+  while (($line = fgets($fh)) !== false) {
+    $match = array();
+    if ($LOOK_FOR_FNAME && preg_match($functionStringCryptPattern,$line,$match)) { 
+      $uncryptStringFunctionName = $match[1];
+	  $LOOK_FOR_DTABLE = true;
+      $LOOK_FOR_FNAME = false;
+    }
+    if ($LOOK_FOR_DTABLE && strpos($line,"local dtable") !== FALSE) {
+	  $dtable = luaStringToPhpString(extractStringFromLine($line));
+	  $LOOK_FOR_DTABLE = false;
+    }
+    if (!$LOOK_FOR_DTABLE && strpos($line,$uncryptStringFunctionName) !== FALSE) {
+      $chaineToDecrypt = luaStringToPhpString(extractStringFromLine(substr($line,strpos($line,$uncryptStringFunctionName))));
+      $decryptedLine = udecrypt($chaineToDecrypt,$dtable);
+ 	  echo "-- ".$decryptedLine.PHP_EOL;
+	}
+	echo $line;
+  }
+
+  fclose($fh);
+
+
