df900a629c022bc46a8a540288b64a333cb920c0
Krevo <francois@crevola.org>
2015-09-25 08:22:08 +0200

    Indent, Space, ...

--- readustring.php
+++ readustring.php
@@ -1,6 +1,6 @@
 <?php 
 
-  function extractStringFromLine($str, $startToken = '"', $endToken = '"')  {
+  function extractStringFromLine($str, $startToken = '"', $endToken = '"') {
     $startIndex = strpos($str, $startToken) + strlen($startToken);
     $endIndex = strrpos($str, $endToken);
     return substr($str, $startIndex, $endIndex - $startIndex);
@@ -19,9 +19,9 @@
     $search[] = "\\\\"; $repl[] = "\\";  
     $search[] = "\\\""; $repl[] = "\"";
     foreach($match[0] as $matched) {
-    	$code = substr($matched,1,3);
-    	$search[] = $matched;
-        $repl[] = chr(intval($code));
+      $code = substr($matched, 1, 3);
+      $search[] = $matched;
+      $repl[] = chr(intval($code));
     }
     return str_replace($search,$repl,$luaStr);
   }
@@ -44,7 +44,7 @@ end
 
   function udecrypt($str,$dtable) {
     $res = "";
-    for ($i=0; $i<strlen($str); $i++) {
+    for ($i = 0; $i < strlen($str); $i++) {
       $b = ord($str[$i]);
       if ($b > 0 && $b <= 127) {
         $res .= $dtable[$b-1];
@@ -56,17 +56,15 @@ end
   }
 
   // Check number of arguments
-  if (count($argv)<2) {
-    echo "Usage: php $argv[0] lua_source_file > lua_source_file_decrypted".PHP_EOL;
-    die;
+  if (count($argv) < 2) {
+    exit("Usage: php $argv[0] lua_source_file > lua_source_file_decrypted".PHP_EOL);
   }
 
   // Open file
   $filename = $argv[1];
   $fh = @fopen($filename,"rb");
   if ($fh === FALSE) {
-    echo "Unable to open $filename !".PHP_EOL;
-    die;
+    exit("Unable to open $filename !".PHP_EOL);
   }
   
   $functionStringCryptPattern = "/function (_[a-zA-Z0-9]{3})\\(/";
@@ -77,21 +75,21 @@ end
   $uncryptStringFunctionName = "____";
   while (($line = fgets($fh)) !== false) {
     $match = array();
-    if ($LOOK_FOR_FNAME && preg_match($functionStringCryptPattern,$line,$match)) { 
+    if ($LOOK_FOR_FNAME && preg_match($functionStringCryptPattern, $line, $match)) { 
       $uncryptStringFunctionName = $match[1];
-	  $LOOK_FOR_DTABLE = true;
+      $LOOK_FOR_DTABLE = true;
       $LOOK_FOR_FNAME = false;
     }
-    if ($LOOK_FOR_DTABLE && strpos($line,"local dtable") !== FALSE) {
-	  $dtable = luaStringToPhpString(extractStringFromLine($line));
-	  $LOOK_FOR_DTABLE = false;
+    if ($LOOK_FOR_DTABLE && strpos($line, "local dtable") !== FALSE) {
+      $dtable = luaStringToPhpString(extractStringFromLine($line));
+      $LOOK_FOR_DTABLE = false;
     }
-    if (!$LOOK_FOR_DTABLE && strpos($line,$uncryptStringFunctionName) !== FALSE) {
+    if (!$LOOK_FOR_DTABLE && strpos($line, $uncryptStringFunctionName) !== FALSE) {
       $results = array();
       preg_match_all('`"([^"]*)"`', $line, $results);
       foreach($results[1] as $item) {
         $chaineToDecrypt = luaStringToPhpString($item);
-        $decryptedLine = udecrypt($chaineToDecrypt,$dtable);
+        $decryptedLine = udecrypt($chaineToDecrypt, $dtable);
         echo "-- ".$decryptedLine.PHP_EOL;
       }
     }
@@ -99,5 +97,4 @@ end
   }
 
   fclose($fh);
-
 
