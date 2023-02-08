945141345b711992276b39e933e5b3066cd33561
Krevo <francois@crevola.org>
2023-02-08 13:33:30 +0100

    Rewrite to stop using BCMath functions - fix #5

--- ucommons.php
+++ ucommons.php
@@ -34,45 +34,14 @@
     return $hash;
   }
 
-  function convBase($numberInput, $fromBaseInput, $toBaseInput)
-  {
-    if ($fromBaseInput==$toBaseInput) return $numberInput;
-    $fromBase = str_split($fromBaseInput,1);
-    $toBase = str_split($toBaseInput,1);
-    $number = str_split($numberInput,1);
-    $fromLen=strlen($fromBaseInput);
-    $toLen=strlen($toBaseInput);
-    $numberLen=strlen($numberInput);
-    $retval='';
-    if ($toBaseInput == '0123456789')
-    {
-        $retval=0;
-        for ($i = 1;$i <= $numberLen; $i++)
-            $retval = bcadd($retval, bcmul(array_search($number[$i-1], $fromBase),bcpow($fromLen,$numberLen-$i)));
-        return $retval;
-    }
-    if ($fromBaseInput != '0123456789')
-        $base10=convBase($numberInput, $fromBaseInput, '0123456789');
-    else
-        $base10 = $numberInput;
-    if ($base10<strlen($toBaseInput))
-        return $toBase[$base10];
-    while($base10 != '0')
-    {
-        $retval = $toBase[bcmod($base10,$toLen)].$retval;
-        $base10 = bcdiv($base10,$toLen,0);
-    }
-    return $retval;
-  }
-
   /*
     This function will yield collisions for the desired hash
   */
   function findHash($hashToFind, $len = 4)
   {
     $max = pow(26, $len);
-    for ($i=0; $i<$max; $i++) {
-      $s = str_pad(convBase($i, '0123456789', 'abcdefghijklmnopqrstuvwxyz'),$len,"a",STR_PAD_LEFT);
+    $s = str_pad('',$len,"a",STR_PAD_LEFT);
+    for ($i=0; $i<$max; $i++, $s++) {
       if (RSHash($s)==$hashToFind) {
         yield $s;
       }
